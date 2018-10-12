356b749fe820183dfdbdaf3b4feee8b5891c61c7
leadjockey <dltldnjs5790@naver.com>
2018-10-12 19:45:24 +0900

    mongoose added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
 const cors = require('cors');
+const db = mongoose.connection;
+const app = express();
+
+app.set('PORT',process.env.PORT || 3000);
+app.set('DB_URL', 'mongodb://localhost/api_practice');
+
+db.on('error', console.error);
+db.once('open', ()=> console.log(`[api-practice-log] Connected to mongo server`));
+mongoose.connect(app.get('DB_URL'));
 
 app.use(express.json());
 app.use(cors());
+app.use('/api/v1/users', require('./api/v1/users'));
+app.use('*', (req, res) => res.status(404).send({error:'page not found'}));
 
-app.use('/api/v1/user', require('./api/v1/user'));
-app.use('/', (req, res) => res.json({msg:'hello world'}));
-
-
-app.listen(port, () => console.log(`server listening on port ${port}`));
+app.listen(app.get('PORT'), () => console.log(`[api-practice-log] Server listening on port ${app.get('PORT')}`));
